38c859b368eda2a6d9f96de4e43b011e3dba6d22
Tempus93 <justindowner121@gmail.com>
2021-07-23 00:05:15 -0400

    commit on ASCProject

--- week2/day4/Collision_Theory_Project/sketch.js
+++ week2/day4/Collision_Theory_Project/sketch.js
@@ -5,27 +5,36 @@ let enYpos = 300;
 
 let mytop,mybottom,myleft,myright;
 let entop,enbottom,enleft,enright;
+let enSize  =25;
+let score = 0;
 
+let img1;
+let img2;
+
+function preload(){
+    img1 = loadImage("Images/poke1.png");
+    img2 = loadImage("Images/poke2.png");
+
+}
 function setup(){
     createCanvas(500,500);
     background(0);
     noStroke();
 
 
-    rectMode(CENTER);
+    imageMode(CENTER);
 
 }
+
 
 function draw(){
     background(0);
 
     // enemy character
-    fill('blue')
-    rect(enXpos,enYpos,50)
+    image(img1,enXpos,enYpos,enSize,enSize);
 
-     // My character
-     fill('red')
-     rect(myXpos,myYpos,50,50)
+    // My character
+    image(img2,myXpos,myYpos,25,25);
 
     // Hitboxes 
     myleft = myXpos - 25;
@@ -33,11 +42,16 @@ function draw(){
     mytop = myYpos - 25;
     mybottom = myYpos + 25;
 
-    enright = enXpos + 25
-    enleft  = enXpos - 25
-    entop = enYpos - 25
-    enbottom = enYpos + 25
+    enright = enXpos + 25;
+    enleft  = enXpos - 25;
+    entop = enYpos - 25;
+    enbottom = enYpos + 25;
     
+    // Scoreboard
+    fill('white');
+    textSize(22);
+    text(`Score: ${score}`,1,20);
+
     // Movement
     if(keyIsDown(LEFT_ARROW)){
         myXpos -= 3;
@@ -56,13 +70,13 @@ function draw(){
     }
 
     // Collision Detection
-    if(myleft > enright || myright < enleft || mytop > enbottom || mybottom < entop){
-        fill('yellow')
-        textSize(22)
-        text("No Collision",190,480);
-    }
+    if (myleft > enright || myright < enleft || mytop > enbottom || mybottom < entop){
 
-    else{    
+    }
+    
+    else {
+        enSize = random(25,100);
+        score += 1;    
         enXpos = random(1,500);
         enYpos = random(1,500);
     }    
