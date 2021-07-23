4fb522ffe027250a4a6ae4161ef96cf3d4076c21
Tempus93 <justindowner121@gmail.com>
2021-07-23 00:34:57 -0400

    commit

--- week2/day4/oop1/sketch.js
+++ week2/day4/oop1/sketch.js
@@ -6,6 +6,20 @@ function setup() {
 
 function draw() {
     background(0);
+
+    let ball1 = new ball(150,250,255,0,0);
+    let ball2 = new ball(250,250,0,255,0);
+    let ball3 = new ball(350,250,0,0,255);
+
+
+    fill(ball1.redValue,ball1.greenValue,ball1.blueValue);
+    ellipse(ball1.Xpos,ball1.Ypos,100);
+
+    fill(ball2.redValue, ball2.greenValue,ball2.blueValue);
+    ellipse(ball2.Xpos,ball2.Ypos,100);
+
+    fill(ball3.redValue,ball3.greenValue,ball3.blueValue);
+    ellipse(ball3.Xpos,ball3.Ypos,100);
 }
  
 class ball {
@@ -18,14 +32,3 @@ class ball {
 
     }
 }
-
-let ball = {
-    Xpos: 100,
-    Ypos: 100,
-    redValue: 255, 
-    greenValue: 0,
-    blueValue: 0
-
-}
-fill(ball.redValue,ball.greenValue,ball.blueValue);
-ellipse(ball.Xpos,ball.Ypos,50);
---
+++ week2/day4/oop2/sketch.js
@@ -0,0 +1,38 @@
+let ballArray = [];
+
+function setup() {
+    createCanvas(500,500);
+
+    for (let i = 0; i < 10; i++){
+        let temp = new ball(random(0,500),250,0,255,0,random(2,5));
+        ballArray.push(temp);
+    }
+}
+
+function draw() {
+    background(0);
+
+    for(let i = 0;i < ballArray.length;i++){
+        fill(ballArray[i].redValue,ballArray[i].greenValue,ballArray[i].blueValue);
+        ellipse(ballArray[i].Xpos,ballArray[i].Ypos,50);
+        ballArray[i].Ypos += ballArray[i].speedValue;
+
+        if(ballArray[i].Ypos > 525){
+            ballArray[i].Ypos = -25;
+        }
+
+    }
+
+}
+
+class ball {
+    constructor(x,y,r,g,b,speed) {
+        this.Xpos = x;
+        this.Ypos = y;
+        this.redValue = r;
+        this.greenValue = g;
+        this.blueValue = b;
+        this.speedValue = speed;
+
+    }
+}
