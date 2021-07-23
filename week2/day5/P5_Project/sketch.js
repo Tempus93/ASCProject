3378486e6a2c746a7da30d3f9b8c12cd2a7cdd42
Tempus93 <justindowner121@gmail.com>
2021-07-23 10:48:32 -0400

    commit

--- week2/day5/P5_Project/sketch.js
+++ week2/day5/P5_Project/sketch.js
@@ -1,15 +1,26 @@
-let bg;
+// Main Variables
 
+let bg; 
+
+// moves
+const moves = ["Fire","Water","Grass"];
+
+// image association
+const images = {
+  Fire: "Charzard",
+  Water: "Blastoise",
+  Grass: "Ivysaur",
+  computer: "trainer",
+  player: "ash"
+};
+
+//score and counter
+let score_player = 0;
+let score_computer = 0;
+let play_counter = 0;
+
+// load the images and background
 function preload() {
-  bg = loadImage('images/background.jpeg');
-  
+   let bg = loadImage("images/background.jpeg")
 }
 
-function setup() {
-  createCanvas(500, 500);
-  background(220);
-}
-
-function draw() {
-    image(bg,0,0);
-}
