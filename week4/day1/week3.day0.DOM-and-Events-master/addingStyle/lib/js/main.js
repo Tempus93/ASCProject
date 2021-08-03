ead660ed0f9aba7177e654155a6a65e2773c8b70
Tempus93 <justindowner121@gmail.com>
2021-08-03 08:49:42 -0400

    commit

---
+++ week4/day1/week3.day0.DOM-and-Events-master/addingStyle/lib/js/main.js
@@ -0,0 +1,8 @@
+let left_side = document.querySelector("left_side");
+
+left_side.getElementsByClassName.backgroundColor = "#d3c26f";
+
+
+function mouseClicked(){
+    left_side.getElementsByClassName.backgroundColor = "#d3c26f";
+};
