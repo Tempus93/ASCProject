cea5cbc42ac4c126f80178f1446f2b7bb3950cf7
Tempus93 <justindowner121@gmail.com>
2021-08-09 00:26:32 -0400

    commit

--- week4/day5/updated Pweb/script.js
+++ week4/day5/updated Pweb/script.js
@@ -1,8 +1,8 @@
 // family meme
 const audio = new Audio;
-audio.src = "family.mp3";
+audio.src = "Audio/family.mp3";
 
-
+// Scrolling down function(s)
 window.addEventListener('scroll',function() {
     let navbar = document.querySelector('.navbar');
     let home = document.querySelector('.home');
@@ -11,3 +11,25 @@ window.addEventListener('scroll',function() {
     home.classList.toggle('scrolled-home', window.scrollY > 1800);
 
 });
+
+// Projects
+function ParsedData(project) {
+    
+    return `
+    <div id = info>
+        <img src ="${project.image}"id ="img">
+        <div id = "text">
+            <div id = "name">${project.name}</div>
+            <div id = "description_title">Description</div>
+            <div id = "description_text">${project.description}</div>
+            <div id = "experience_title">Experience Gained?</div>
+            <div id = "experience_text">${project.experience}</div>
+        </div>
+        
+    </div>
+    `
+}
+
+document.querySelector("#project_containers").innerHTML = `
+ ${projects.results.map(ParsedData).join(" ")}
+`
