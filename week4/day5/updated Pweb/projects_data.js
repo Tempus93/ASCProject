80ef968adcdfbb163ef8dbc0284f711f3d03b5a9
Tempus93 <justindowner121@gmail.com>
2021-08-13 11:07:19 -0400

    commit

---
+++ week4/day5/updated Pweb/projects_data.js
@@ -0,0 +1,36 @@
+//Projects data
+
+
+// Template
+//    {
+//    "name":"",
+//    "image":"images/",
+//    "description":"",
+//    "experience":""
+//    },
+//
+
+
+
+const projects = {
+    "results":[
+        {
+        "name": "Visual Data",
+        "image":"images/projectVD.png",
+        "description":"Making use of randomly generated data to create a website that list all persons, along with their important contact information.",
+        "experience":"Learned how to use data from JSON format(JS) and parse them onto HTML to create websites.(This is how I created my projects page!!)"
+        },
+
+        {
+        "name": "Vetted API",
+        "image": "images/Vetted_API.PNG",
+        "description":"Made use of a pokemon API to generate the name, base stats and image of any pokemon whose name is typed.",
+        "experience":"Learned how to use fetch to recieve API JSON Data and parse it to be used for project use."
+        },
+
+        
+
+    ],
+
+
+}
